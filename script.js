264cb500ed52f0cf3ec98b9b1866f7c16cfcfd9c
Fabian Higuita <fabianes1996@gmail.com>
2022-02-09 11:54:40 -0500

    Add challenge 3

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-'use strict'
+"use strict";
 //Coding Challenge 1
 /* 
 We're building a football betting app (soccer for my American friends 😅)!
@@ -14,39 +14,39 @@ TEST DATA FOR 6: Use players 'Davies', 'Muller', 'Lewandowski' and 'Kimmich'. Th
 GOOD LUCK 😀
 */
 const game = {
-  team1: 'Bayern Munich',
-  team2: 'Borrussia Dortmund',
+  team1: "Bayern Munich",
+  team2: "Borrussia Dortmund",
   players: [
     [
-      'Neuer',
-      'Pavard',
-      'Martinez',
-      'Alaba',
-      'Davies',
-      'Kimmich',
-      'Goretzka',
-      'Coman',
-      'Muller',
-      'Gnarby',
-      'Lewandowski',
+      "Neuer",
+      "Pavard",
+      "Martinez",
+      "Alaba",
+      "Davies",
+      "Kimmich",
+      "Goretzka",
+      "Coman",
+      "Muller",
+      "Gnarby",
+      "Lewandowski",
     ],
     [
-      'Burki',
-      'Schulz',
-      'Hummels',
-      'Akanji',
-      'Hakimi',
-      'Weigl',
-      'Witsel',
-      'Hazard',
-      'Brandt',
-      'Sancho',
-      'Gotze',
+      "Burki",
+      "Schulz",
+      "Hummels",
+      "Akanji",
+      "Hakimi",
+      "Weigl",
+      "Witsel",
+      "Hazard",
+      "Brandt",
+      "Sancho",
+      "Gotze",
     ],
   ],
-  score: '4:0',
-  scored: ['Lewandowski', 'Gnarby', 'Lewandowski', 'Hummels'],
-  date: 'Nov 9th, 2037',
+  score: "4:0",
+  scored: ["Lewandowski", "Gnarby", "Lewandowski", "Hummels"],
+  date: "Nov 9th, 2037",
   odds: {
     team1: 1.33,
     x: 3.25,
@@ -56,18 +56,18 @@ const game = {
 
 //Sol
 
-const [players1,players2] = game.players;
+const [players1, players2] = game.players;
 //console.log(players1,players2);
 
-const [gk,...fieldPlayers] = players1;
+const [gk, ...fieldPlayers] = players1;
 //console.log(go,fieldPlayers);
-const allPlayers = [...players1,...players2];
+const allPlayers = [...players1, ...players2];
 //console.log(allPlayers);
 
-const players1Final = [...players1,'Thiago','Coutinho','Perisic'];
+const players1Final = [...players1, "Thiago", "Coutinho", "Perisic"];
 //console.log(players1Final);
 
-const {team1,x:draw,team2} = game.odds
+const { team1, x: draw, team2 } = game.odds;
 
 //console.log(team1,draw,team2);
 
@@ -80,9 +80,8 @@ const printGoals = function (...players) {
 // printGoals('Davies', 'Muller');
 printGoals(...game.scored);
 
-team1 < team2 && console.log('Team 1 is more likely to win');
-team1 > team2 && console.log('Team 2 is more likely to win');
-
+team1 < team2 && console.log("Team 1 is more likely to win");
+team1 > team2 && console.log("Team 2 is more likely to win");
 
 ///////////////////////////////////////
 // Coding Challenge #2
@@ -107,29 +106,93 @@ GOOD LUCK 😀
 
 console.log("Challenge 2");
 //1
-for(const i of game.scored.entries()){
+for (const i of game.scored.entries()) {
   console.log(`Goal ${i[0] + 1}: ${i[1]}`);
 }
 
 //2
 let avg = 0;
-const odds = Object.values(game.odds)
-for(const odd of odds){
+const odds = Object.values(game.odds);
+for (const odd of odds) {
   avg += odd;
 }
-avg = avg/odds.length
+avg = avg / odds.length;
 console.log(avg);
 
 //3
-for(const team of Object.entries(game.odds)){
-  console.log(`Odd of ${game[team[0]] || 'draw'}: ${team[1]}`  );
+for (const team of Object.entries(game.odds)) {
+  console.log(`Odd of ${game[team[0]] || "draw"}: ${team[1]}`);
 }
 
 //Bonus
 console.log(game.scored);
 
-let scorers ={}
-for(const player of game.scored){
-  !scorers[player] ? scorers[player] = 1 : scorers[player] += 1;
+let scorers = {};
+for (const player of game.scored) {
+  !scorers[player] ? (scorers[player] = 1) : (scorers[player] += 1);
 }
 console.log(scorers);
+
+/*Map challenge */
+
+// const answerMap = new Map([
+//   ["question", "What's the best programing lenguage"],
+//   [1, "C"],
+//   [2, "Java"],
+//   [3, "Javascript"],
+//   ['correct', 3],
+//   [true, "Correct"],
+//   [false, "Incorrect"],
+// ]);
+
+// console.log(answerMap.get("question"));
+// for(const [key,value] of answerMap){
+//   if(typeof(key) ==='number') console.log(`Answer ${key}: ${value}`);
+// }
+
+// const answer = Number(prompt('Your answer'));
+
+// console.log(answerMap.get(answer === 3));
+
+///////////////////////////////////////
+// Coding Challenge #3
+
+/* 
+Let's continue with our football betting app! This time, we have a map with a log of the events that happened during the game. The values are the events themselves, and the keys are the minutes in which each event happened (a football game has 90 minutes plus some extra time).
+1. Create an array 'events' of the different game events that happened (no duplicates)
+2. After the game has finished, is was found that the yellow card from minute 64 was unfair. So remove this event from the game events log.
+3. Print the following string to the console: "An event happened, on average, every 9 minutes" (keep in mind that a game has 90 minutes)
+4. Loop over the events and log them to the console, marking whether it's in the first half or second half (after 45 min) of the game, like this:
+      [FIRST HALF] 17: ⚽️ GOAL
+GOOD LUCK 😀
+*/
+
+console.log("-------Challenge 3 --------");
+const gameEvents = new Map([
+  [17, '⚽️ GOAL'],
+  [36, '🔁 Substitution'],
+  [47, '⚽️ GOAL'],
+  [61, '🔁 Substitution'],
+  [64, '🔶 Yellow card'],
+  [69, '🔴 Red card'],
+  [70, '🔁 Substitution'],
+  [72, '🔁 Substitution'],
+  [76, '⚽️ GOAL'],
+  [80, '⚽️ GOAL'],
+  [92, '🔶 Yellow card'],
+]);
+//1
+const events = [...new Set(gameEvents.values())]
+console.log(events);
+
+//2
+gameEvents.delete(64);
+console.log(gameEvents);
+
+//3
+console.log(`An event happend, on avarage ${90 / gameEvents.size} minutes`);
+
+//4
+for(const [key, value] of gameEvents){
+  console.log(`${key<45 ? '[FIRST HALF]': '[SECOND HALF]'} ${key}: ${value}`);
+}
