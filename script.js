ab811c9d0b09b990e951a44e4f339965252e4053
Fabian Higuita <fabianes1996@gmail.com>
2022-02-10 10:32:30 -0500

    Add challenge 1 -section 11

--- script.js
+++ script.js
@@ -61,6 +61,23 @@ const inputLoanAmount = document.querySelector('.form__input--loan-amount');
 const inputCloseUsername = document.querySelector('.form__input--user');
 const inputClosePin = document.querySelector('.form__input--pin');
 
+const displayMovements = function(movements){
+  containerMovements.innerHTML = ''
+  movements.forEach((mov,i) => {
+    const type = mov > 0 ? 'deposit' : 'withdrawal'
+
+    const html = `
+    <div class="movements__row">
+      <div class="movements__type movements__type--${type}">${i+1} ${type}</div>      
+      <div class="movements__value">${mov}</div>
+    </div>
+    `;
+    containerMovements.insertAdjacentHTML('afterbegin',html);
+  });
+}
+
+displayMovements(account1.movements)
+
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
 // LECTURES
@@ -74,3 +91,37 @@ const currencies = new Map([
 const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 /////////////////////////////////////////////////
+
+///////////////////////////////////////
+// Coding Challenge #1
+
+/* 
+Julia and Kate are doing a study on dogs. So each of them asked 5 dog owners about their dog's age, and stored the data into an array (one array for each). For now, they are just interested in knowing whether a dog is an adult or a puppy. A dog is an adult if it is at least 3 years old, and it's a puppy if it's less than 3 years old.
+Create a function 'checkDogs', which accepts 2 arrays of dog's ages ('dogsJulia' and 'dogsKate'), and does the following things:
+1. Julia found out that the owners of the FIRST and the LAST TWO dogs actually have cats, not dogs! So create a shallow copy of Julia's array, and remove the cat ages from that copied array (because it's a bad practice to mutate function parameters)
+2. Create an array with both Julia's (corrected) and Kate's data
+3. For each remaining dog, log to the console whether it's an adult ("Dog number 1 is an adult, and is 5 years old") or a puppy ("Dog number 2 is still a puppy 🐶")
+4. Run the function for both test datasets
+HINT: Use tools from all lectures in this section so far 😉
+TEST DATA 1: Julia's data [3, 5, 2, 12, 7], Kate's data [4, 1, 15, 8, 3]
+TEST DATA 2: Julia's data [9, 16, 6, 8, 3], Kate's data [10, 5, 6, 1, 4]
+GOOD LUCK 😀
+*/
+
+const data1 = [3,5,2,12,7]
+const data2 = [4,1,15,8,3]
+const data3 = [9,16,6,8,3]
+const data4 = [10,5,6,1,4]
+
+const checkDogs = (dogsJulia,dogsKate)=>{
+  let copy = [...dogsJulia];
+  copy= copy.slice(1,-2);
+  const realDogs = [...copy, ...dogsKate];
+  realDogs.forEach((dog,i)=>{
+    dog >= 3  ? console.log(`Dog number ${i+1} is an adult, and is ${dog} years old`)
+              : console.log(`Dog number ${i+1} is still a puppy 🐶`);
+  }) 
+}
+
+checkDogs(data1,data2)
+checkDogs(data3,data4)
