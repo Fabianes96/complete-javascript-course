8bfb3d6da024e0f2e573f112aa5fcea06b2d7e5f
Fabian Higuita <fabianes1996@gmail.com>
2022-02-10 12:10:54 -0500

    Add login logic

--- script.js
+++ script.js
@@ -76,7 +76,7 @@ const displayMovements = function(movements){
   });
 }
 
-displayMovements(account1.movements)
+
 
 const calcDisplayBalance = function(movements){
   const balance = movements.reduce((acc,mov)=>{
@@ -85,24 +85,23 @@ const calcDisplayBalance = function(movements){
   labelBalance.textContent = `${balance}€`
 }
 
-const calcDisplaySummary = (movements)=>{
-  const incomes = movements
+const calcDisplaySummary = (acc)=>{
+  const incomes = acc.movements
     .filter(mov => mov >0)
     .reduce((acc,mov)=>acc +mov, 0)
   labelSumIn.textContent = `${incomes}€`
-  const out = movements
+  const out = acc.movements
     .filter(mov => mov < 0)
     .reduce((acc,mov)=>acc +mov,0)
   labelSumOut.textContent = `${Math.abs(out)}€`
 
-  const interest = movements
+  const interest = acc.movements
     .filter(mov => mov >0)
-    .map(deposit => deposit*1.2/100)
+    .map(deposit => deposit*acc.interestRate/100)
     .filter((int,i,arr)=>int>=1)
     .reduce((acc,int)=>acc +int,0);
   labelSumInterest.textContent = `${interest}€`
 }
-calcDisplaySummary(account1.movements)
 
 const createUsernames = function (accs){
   accs.forEach((acc=>{
@@ -113,9 +112,28 @@ const createUsernames = function (accs){
     .join('');
   }))  
 }
-calcDisplayBalance(account1.movements);
 createUsernames(accounts)
 
+let currentAcount;
+
+btnLogin.addEventListener('click',function(e){
+  e.preventDefault();
+  currentAcount= accounts.find(acc => acc.username === inputLoginUsername.value)
+  console.log(currentAcount);
+  if(currentAcount?.pin === Number(inputLoginPin.value)){
+    labelWelcome.textContent = `Welcome back, ${currentAcount.owner.split(' ')[0]}`
+  }
+  containerApp.style.opacity = 100;
+  //Clear the input fields
+
+  inputLoginUsername.value = inputLoginPin.value=''
+  inputLoginPin.blur();
+
+
+  displayMovements(currentAcount.movements)
+  calcDisplayBalance(currentAcount.movements);
+  calcDisplaySummary(currentAcount)
+})
 
 
 
@@ -182,7 +200,7 @@ TEST DATA 2: [16, 6, 10, 5, 6, 1, 4]
 GOOD LUCK 😀
 */
 
-
+console.log("--------------CHALLENGE 2 ------------");
 const calcAverageHumanAge = (ages)=>{
   const humanAge = ages.map((age)=>{
     if(age<=2){
@@ -201,3 +219,26 @@ const calcAverageHumanAge = (ages)=>{
 }
 calcAverageHumanAge([5, 2, 4, 1, 15, 8, 3])
 calcAverageHumanAge([16, 6, 10, 5, 6, 1, 4])
+
+
+//////////////////////////////////////
+// Coding Challenge #3
+
+/* 
+Rewrite the 'calcAverageHumanAge' function from the previous challenge, but this time as an arrow function, and using chaining!
+TEST DATA 1: [5, 2, 4, 1, 15, 8, 3]
+TEST DATA 2: [16, 6, 10, 5, 6, 1, 4]
+GOOD LUCK 😀
+*/
+
+console.log("----------------CHALLENGE 3--------------------------");
+
+const calcAverageHumanAgeChaining = (ages)=>{
+  const humanAge = ages
+    .map( age=>(age <= 2 ? 2*age : 16 + age*4))
+    .filter(age => age >=18)
+    .reduce((acc,age,i,arr)=> acc + age/ arr.length,0)  
+  return humanAge
+}
+console.log("Chaining", calcAverageHumanAgeChaining([5, 2, 4, 1, 15, 8, 3]));
+console.log("Chaining", calcAverageHumanAgeChaining([16, 6, 10, 5, 6, 1, 4]));
