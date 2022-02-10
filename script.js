df0d1102c283dc42d5bcb2e34cbed55a0f53dc83
Fabian Higuita <fabianes1996@gmail.com>
2022-02-10 11:42:02 -0500

    Add calcDisplaySummary function

--- script.js
+++ script.js
@@ -69,7 +69,7 @@ const displayMovements = function(movements){
     const html = `
     <div class="movements__row">
       <div class="movements__type movements__type--${type}">${i+1} ${type}</div>      
-      <div class="movements__value">${mov}</div>
+      <div class="movements__value">${mov} €</div>
     </div>
     `;
     containerMovements.insertAdjacentHTML('afterbegin',html);
@@ -82,8 +82,27 @@ const calcDisplayBalance = function(movements){
   const balance = movements.reduce((acc,mov)=>{
     return acc +mov;
   },0)
-  labelBalance.textContent = `${balance} EUR`
+  labelBalance.textContent = `${balance}€`
 }
+
+const calcDisplaySummary = (movements)=>{
+  const incomes = movements
+    .filter(mov => mov >0)
+    .reduce((acc,mov)=>acc +mov, 0)
+  labelSumIn.textContent = `${incomes}€`
+  const out = movements
+    .filter(mov => mov < 0)
+    .reduce((acc,mov)=>acc +mov,0)
+  labelSumOut.textContent = `${Math.abs(out)}€`
+
+  const interest = movements
+    .filter(mov => mov >0)
+    .map(deposit => deposit*1.2/100)
+    .filter((int,i,arr)=>int>=1)
+    .reduce((acc,int)=>acc +int,0);
+  labelSumInterest.textContent = `${interest}€`
+}
+calcDisplaySummary(account1.movements)
 
 const createUsernames = function (accs){
   accs.forEach((acc=>{
