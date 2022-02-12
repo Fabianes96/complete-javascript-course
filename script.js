a66b79a220fc78ed767cdb79dbcb95f60dc1e9b4
Fabian Higuita <fabianes1996@gmail.com>
2022-02-12 17:18:38 -0500

    Add challenges 3 & 4

--- script.js
+++ script.js
@@ -45,10 +45,12 @@ class Car2 {
   accelerate(){
     this.speed +=10;
     console.log(`${this.make} is goint at ${this.speed} k/h`);
+    return this;
   }
   brake(){
     this.speed -= 5;
     console.log(`${this.make} is goint at ${this.speed} k/h`);
+    return this;
   }
   get speedUS(){
     return this.speed / 1.6
@@ -86,3 +88,83 @@ Student.prototype.introduce = function(){
 const mike = new Student('mike',2020, 'Computer Science');
 mike.introduce()
 
+///////////////////////////////////////
+// Coding Challenge #3
+
+/* 
+1. Use a constructor function to implement an Electric Car (called EV) as a CHILD "class" of Car. Besides a make and current speed, the EV also has the current battery charge in % ('charge' property);
+2. Implement a 'chargeBattery' method which takes an argument 'chargeTo' and sets the battery charge to 'chargeTo';
+3. Implement an 'accelerate' method that will increase the car's speed by 20, and decrease the charge by 1%. Then log a message like this: 'Tesla going at 140 km/h, with a charge of 22%';
+4. Create an electric car object and experiment with calling 'accelerate', 'brake' and 'chargeBattery' (charge to 90%). Notice what happens when you 'accelerate'! HINT: Review the definiton of polymorphism 😉
+DATA CAR 1: 'Tesla' going at 120 km/h, with a charge of 23%
+GOOD LUCK 😀
+*/
+
+const EV = function(make,speed,charge){
+  Car.call(this,make,speed);
+  this.charge = charge;
+}
+
+EV.prototype = Object.create(Car.prototype);
+
+EV.prototype.chargeBattery = function(chargeTo){
+  this.charge = chargeTo;
+  console.log('Current charge', this.charge);
+}
+EV.prototype.accelerate = function(){
+  this.speed += 10;
+  this.charge -= this.charge*0.01
+
+  console.log(`${this.make} going at ${this.speed}, with charge of ${this.charge}%`);
+}
+
+
+console.log('Challenge 3');
+
+const tesla = new EV('Tesla', 120, 23);
+tesla.chargeBattery(90);
+console.log(tesla);
+tesla.brake();
+console.log(tesla);
+tesla.accelerate();
+
+
+
+///////////////////////////////////////
+// Coding Challenge #4
+
+/* 
+1. Re-create challenge #3, but this time using ES6 classes: create an 'EVCl' child class of the 'CarCl' class
+2. Make the 'charge' property private;
+3. Implement the ability to chain the 'accelerate' and 'chargeBattery' methods of this class, and also update the 'brake' method in the 'CarCl' class. They experiment with chining!
+DATA CAR 1: 'Rivian' going at 120 km/h, with a charge of 23%
+GOOD LUCK 😀
+*/
+
+
+class EVCL extends Car2{
+  #charge;
+  constructor(make, speed, charge){
+    super(make,speed)
+    this.#charge = charge;
+  }
+  chargeBattery(chargeTo){
+    this.#charge = chargeTo
+    return this
+  }
+  accelerate(){
+    this.speed += 20;
+    this.#charge--;
+    console.log(`${this.make} is going at ${this.speed} km/h, with a charge of ${this.#charge}`);
+    return this;
+  }
+}
+
+const rivian = new EVCL('Rivian',120,23);
+rivian
+  .accelerate()
+  .accelerate()
+  .accelerate()
+  .brake()
+  .chargeBattery(50)
+  .accelerate()
